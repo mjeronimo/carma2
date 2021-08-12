13aa7184851058dc5d875d752557d4a315d03c04
Patrick Musau <pmusau13ster@gmail.com>
2021-08-12 11:37:36 -0500

    exception spinning

--- carma_platform/subsystems/perception/carma_velodyne_lidar_driver/src/main.cpp
+++ carma_platform/subsystems/perception/carma_velodyne_lidar_driver/src/main.cpp
@@ -24,7 +24,6 @@ int main(int argc, char ** argv)
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<carma_velodyne_lidar_driver::CarmaVelodyneLidarDriver>();
-  // rclcpp::spin(node->get_node_base_interface());
   node->spin();
 
   rclcpp::shutdown();
